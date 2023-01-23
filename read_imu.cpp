8151ad0cd9e8b7394730fdd3a17ac0cb6d968f7e
Luke <lazarluke02@gmail.com>
2023-01-24 03:09:24 +1100

    bug

--- read_imu.cpp
+++ read_imu.cpp
@@ -10,12 +10,17 @@ int main(){
     fd = wiringPiI2CSetup(0x1d);
 
     std::cout << fd << std::endl;
+
+    if (fd == -1){
+        std::cout << "Could not read address" << std::endl;
+        exit(1);
+    }
     
 
     while(1){
         std::cout << wiringPiI2CRead(fd) << std::endl;
 
-
+        std::cout << fd << std::endl;
     }
 }
 
