7fd95494afa0b9b49016358af0ff961522cb1281
Luke <lazarluke02@gmail.com>
2023-01-24 03:00:55 +1100

    bug

--- read_imu.cpp
+++ read_imu.cpp
@@ -10,10 +10,13 @@ int main(){
     fd = wiringPiI2CSetup(0x1d);
 
     std::cout << fd << std::endl;
-
-    while(1){
+    
+    int j = 1
+    while(j){
         std::cout << wiringPiI2CRead(fd) << std::endl;
+
+        std::in >> j;
     }
 }
 
-//g++ read_imu.cpp -l wiringPi -o i2ctest
+//g++ read_imu.cpp -l wiringPi -o read
