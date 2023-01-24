3c94b09909efc851d030d416bee3aa3b21d35e14
Luke <lazarluke02@gmail.com>
2023-01-24 15:15:40 +1100

    test

--- i2c_imu.py
+++ i2c_imu.py
@@ -17,17 +17,14 @@ if not MMAB452Q in i2c.scan():
 
 def get_MMAB452Q_id():
 
-    for i in range(255):
+    # for i in range(0xFF):
 
-        # hex_int = int(hex(i), base=16)
-        # new_int = hex_int + 0x200
 
-        # print(hex_int)
-
-        i2c.writeto(MMAB452Q,  bytes([i]), stop = False) 
+        i2c.writeto(MMAB452Q,  bytes([0xFF]), stop = False) 
         result = bytearray(1)
         i2c.readfrom_into(MMAB452Q, result)
 
         print("ID: ", int.from_bytes(result, "big"))
+
 
 get_MMAB452Q_id()
