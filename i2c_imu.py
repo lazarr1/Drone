f786597421dbc39304a6d13185c15807fa61a657
Luke Lazar <74656988+lazarr1@users.noreply.github.com>
2023-07-07 15:18:37 +1000

    i2c

--- i2c_imu.py
+++ i2c_imu.py
@@ -15,34 +15,31 @@ numb = 1
 bus.write_byte(addr, 0xFF)
 
 
-for i in range(1,20):
-    value= bus.read_byte(addr)
+for i in range(1, 20):
+    value = bus.read_byte(addr)
 
     print(value)
     time.sleep(1)
 
 
-    
+i2c = busio.I2C(board.SCL, board.SDA)
 
 
+print("Scan : ", [hex(i) for i in i2c.scan()])
 
-# i2c = busio.I2C(board.SCL, board.SDA)
+MMAB452Q = 0x1d
 
-
-# print("Scan : " , [hex(i) for i in i2c.scan()])
-
-# MMAB452Q = 0x1d
-
-# if not MMAB452Q in i2c.scan():
-#     print("could not find imu")
-#     sys.exit()
+if MMAB452Q not in i2c.scan():
+    print("could not find imu")
+    sys.exit()
 
 
-# def get_MMAB452Q_id():
-#     i2c.writeto(MMAB452Q,  bytes([0x0D]), stop = False) 
-#     result = bytearray(1)
-#     i2c.readfrom_into(MMAB452Q, result)
+def get_MMAB452Q_id():
+    i2c.writeto(MMAB452Q, bytes([0x0D]), stop=False)
+    result = bytearray(1)
+    i2c.readfrom_into(MMAB452Q, result)
 
-#     print("ID: ", int.from_bytes(result, "big"))
+    print("ID: ", int.from_bytes(result, "big"))
 
-# get_MMAB452Q_id()
+
+get_MMAB452Q_id()
